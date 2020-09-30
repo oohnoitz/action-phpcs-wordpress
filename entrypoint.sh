1e7a2d20483caac7eae185f182fe5b373301ed4a
Hung Tran <hung@0x0.ninja>
2020-09-30 18:17:35 -0500

    add quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
   --report-checkstyle \
   ${INPUT_PHPCS_ARGS:-\.} \
   | reviewdog -f="checkstyle" \
-      -name=${INPUT_TOOL_NAME} \
+      -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
