3ac4444ce7ef31fd564d725bb1e5bcfd606b7ab1
Hung Tran <hung@0x0.ninja>
2022-04-14 09:41:15 -0500

    address git changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
