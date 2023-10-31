8e28684faf0f328b6e3ca5697e1c45add85cc502
Hung Tran <hungtran7@proton.me>
2023-10-31 09:43:10 -0500

    fix: arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,11 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 /root/.composer/vendor/bin/phpcs \
   --report-checkstyle \
   --standard="${INPUT_PHPCS_STANDARD}" \
-  "${INPUT_PHPCS_ARGS:-\.}" |
+  ${INPUT_PHPCS_ARGS:-\.} |
   reviewdog -f="checkstyle" \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER:-github-pr-check}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
-    "${INPUT_REVIEWDOG_FLAGS}"
+    ${INPUT_REVIEWDOG_FLAGS}
